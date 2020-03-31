74f8c1c4bb9621ca2aacb5634407a25613219f8a
recchan13 <93rezao@gmail.com>
2020-03-31 20:17:33 +0700

    cari x y z dengan rumus, dan bandingkan dengan index 5 6 7

--- 14117062.cpp
+++ 14117062.cpp
@@ -2,11 +2,15 @@
 14117062*/
 
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 bool cekInput (string input);
 bool cekPanjang (string input);
 string hapusSpasi (string input);
+bool koreksi(int p, int q, int r, string input, int perulangan);
+bool bandingkan(int index, string input, int perulangan, bool hasil);
+void kebenaran();
 
 int main(){
 	string input;
@@ -20,6 +24,20 @@ int main(){
 			cout<<"Anda tidak menginputkan Binary code dengan benar\nMohon inputkan kode biner dengan panjang kelipatan 7";
 		}else{
 			//mulai 
+			int ulang=input.length()/7;
+			int perulangan=ulang-1;
+			
+			for(int i=0;i<=perulangan;i++){
+				bool x = koreksi(0,1,3,input,perulangan);
+				bool y = koreksi(0,2,3,input,perulangan);
+				bool z = koreksi(1,2,3,input,perulangan);
+				
+				x=bandingkan(4, input, perulangan, x);
+				y=bandingkan(5, input, perulangan, y);
+				z=bandingkan(6, input, perulangan, z);
+				
+				kebenaran();
+			}
 		}
 	}
 	
@@ -52,4 +70,38 @@ string hapusSpasi (string input){
 	return baru;
 }
 
+bool koreksi(int p, int q, int r, string input, int perulangan){
+	bool hasil;
+	
+	if ((input[(perulangan*7)+p]) != (input[(perulangan*7)+q])){
+		if ((input[(perulangan*7)+r]) != '1'){
+			return true;
+		}else{
+			return false;
+		}
+	}else{
+		if ((input[(perulangan*7)+r]) != '0'){
+			return true;
+		}else{
+			return false;
+		}
+	}
+}
 
+bool bandingkan(int index, string input, int perulangan, bool hasil){
+//	cout<<input[(perulangan*7)+index];
+//	cout<<hasil<<'\n';
+	
+	bool hasil2=(input[(perulangan*7)+index]);
+	
+	if (hasil2 == hasil){
+		return true;
+	}else{
+//		cout<<hasil;
+		return false;
+	}
+}
+
+void kebenaran(){
+	
+}
