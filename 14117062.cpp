46d1a1789a71b06f86b019574beb5bb385372ce9
recchan13 <93rezao@gmail.com>
2020-03-31 23:24:07 +0700

    tampilkan code yang eror dan benarkan

--- 14117062.cpp
+++ 14117062.cpp
@@ -10,10 +10,11 @@ bool cekPanjang (string input);
 string hapusSpasi (string input);
 bool koreksi(int p, int q, int r, string input, int perulangan);
 bool bandingkan(int index, string input, int perulangan, bool hasil);
-void kebenaran(bool x,bool y,bool z);
+int kebenaran(bool x,bool y,bool z, string input, int perulangan);
 
 int main(){
 	string input;
+		cout<<"Masukkan kode : ";
 		getline(cin,input);
 	
 	if (!cekInput(input)){
@@ -23,23 +24,45 @@ int main(){
 		if(!cekPanjang(input)){
 			cout<<"Anda tidak menginputkan Binary code dengan benar\nMohon inputkan kode biner dengan panjang kelipatan 7";
 		}else{
-			int ulang=input.length()/7;
-			int perulangan=ulang-1;
+			int perulangan=(input.length()/7)-1;
 			
 			for(int i=0;i<=perulangan;i++){
-				bool x = koreksi(0,1,3,input,perulangan);
-				bool y = koreksi(0,2,3,input,perulangan);
-				bool z = koreksi(1,2,3,input,perulangan);
+				cout<<"\nKoreksi blok kode ke-"<<i+1;
 				
-				x=bandingkan(4, input, perulangan, x);
-				y=bandingkan(5, input, perulangan, y);
-				z=bandingkan(6, input, perulangan, z);
+				bool x = koreksi(0,1,3,input,i);
+				bool y = koreksi(0,2,3,input,i);
+				bool z = koreksi(1,2,3,input,i);
 				
-				kebenaran(x,y,z);
+				x=bandingkan(4, input, i, x);
+				y=bandingkan(5, input, i, y);
+				z=bandingkan(6, input, i, z);
+				
+				int salah=kebenaran(x,y,z,input,i);
+				
+				if (salah>=0){
+					if (input[salah] == '1'){
+						input[salah]='0';
+					}else{
+						input[salah]='1';	
+					}
+				}
+				
+				cout<<"Kode yang benar : ";
+				for(int j=(i*7);j<((i*7)+7);j++){
+					cout<<input[j];
+				}cout<<"\n";
+			}
+			
+			cout<<"\nKeseluruhan kode yang benar : ";
+			for(int i=0;i<input.length();i++){
+				if ((i+1)%7==0){
+					cout<<input[i]<<" ";
+				}else{
+					cout<<input[i];
+				}
 			}
 		}
 	}
-	
 }
 
 bool cekInput(string input){
@@ -88,16 +111,19 @@ bool koreksi(int p, int q, int r, string input, int perulangan){
 }
 
 bool bandingkan(int index, string input, int perulangan, bool hasil){
-	bool hasil2=(input[(perulangan*7)+index]);
+	int hasil2=(int)input[(perulangan*7)+index]-48;
 	
-	if (hasil2 == hasil){
+	if (hasil == hasil2){
 		return true;
 	}else{
 		return false;
 	}
 }
 
-void kebenaran(bool x,bool y,bool z){
+int kebenaran(bool x,bool y,bool z, string input, int perulangan){
+	int index=-999;
+	
+	cout<<"\nError: ";
 	if (x == 1){
 		if(y == 1){
 			if (z == 1){
@@ -105,27 +131,36 @@ void kebenaran(bool x,bool y,bool z){
 				cout<<"Tidak ada kode yang salah";
 			}else{
 				cout<<"Z error";
+				index=6;
 			}
 		}else{
 			if (z == 1){
 				cout<<"Y error";
+				index=5;
 			}else{
 				cout<<"C error";
+				index=2;
 			}
 		}
 	}else{
 		if(y == 1){
 			if (z == 1){
 				cout<<"X error";
+				index=4;
 			}else{
 				cout<<"B error";
+				index=1;
 			}
 		}else{
 			if (z == 1){
 				cout<<"A error";
+				index=0;
 			}else{
 				cout<<"D error";
+				index=3;
 			}
 		}
 	}
+	cout<<"\n";
+	return ((perulangan*7)+index);
 }
